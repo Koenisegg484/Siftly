9cd7a56766d6f33f85a21b18fdd6a5b26a7f6c88
Shivam Chouhan <shivamc86@gmail.com>
2024-09-23 18:22:14 +0530

    Making database for ML

--- WebScrapper/MainWebScrapper.py
+++ WebScrapper/MainWebScrapper.py
@@ -1,6 +1,6 @@
+import os
+import csv
 from time import sleep
-
-from selenium.common import NoSuchElementException
 
 # MongoDB Imports
 from MongoConnect import get_db
@@ -9,86 +9,126 @@ from MongoConnect import get_db
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from WebScrapper.middlewares_fns import ratings, prices_formatter
 
-# Connecting to Mongo Database, this refers to the EcommStores doc reference
+
+def element_exists(products, by, value):
+    try:
+        products.find_element(by, value)
+        return True
+    except NoSuchElementException:
+        return False
+
+
+
+def add_to_database(product):
+    # Define the filename and directory
+    filename = '../database/ProductsRecommendationsForML.csv'
+    directory = os.path.dirname(filename)
+
+    # Create the directory if it doesn't exist
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Open the file in append mode
+    with open(filename, "a+", newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=product.keys())
+
+        if file.tell() == 0:
+            writer.writeheader()
+
+        writer.writerow(product)
+
+
+# Connecting to Mongo Database, this refers to the Ecommerce Stores doc reference
 ecommstores = get_db()
 
-documents = ecommstores.find()
+def main_web_scrapper():
 
-print (documents)
+    documents = ecommstores.find()
 
-serchQuery = str(input("Enter product to search : "))
+    print (documents)
 
-for doc in documents:
-    print(doc)
+    searchQuery = str(input("Enter product to search : "))
 
-    # Initialising web driver for scrapping
-    chrome_options = Options()
-    chrome_options.page_load_strategy = 'none'
-    driver = webdriver.Chrome(service=Service(), options=chrome_options)
-    driver.maximize_window()
+    for doc in documents:
+        print(doc)
 
-    url = doc['homepage']
-    # print(f"Printing URL => {url}")
+        # Initialising web driver for scrapping
+        chrome_options = Options()
+        chrome_options.page_load_strategy = 'none'
+        driver = webdriver.Chrome(service=Service(), options=chrome_options)
+        driver.maximize_window()
 
-    driver.get(url)
-    sleep(3)
+        url = doc['homepage']
+        # print(f"Printing URL => {url}")
 
-    searchbar_tags = doc['searching_page_tags']['searchbar'][0]
-    searchBar = driver.find_element(By.XPATH, searchbar_tags['xpath'])
-    searchBar.send_keys(serchQuery)
-    searchBar.send_keys(Keys.ENTER)
-    sleep(3)
+        driver.get(url)
+        sleep(3)
 
-    productgrid_tags = doc['searching_page_tags']['productgrid'][0]
-    productlist_tags = doc['searching_page_tags']['productlist'][0]
+        searchbar_tags = doc['searching_page_tags']['searchbar'][0]
+        searchBar = driver.find_element(By.XPATH, searchbar_tags['xpath'])
+        searchBar.send_keys(searchQuery)
+        searchBar.send_keys(Keys.ENTER)
+        sleep(3)
 
-    try:
-
-        productgrid = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.XPATH, productgrid_tags['xpath']))
-        )
-        productlist = productgrid.find_elements(By.CLASS_NAME, productlist_tags['class'])
+        productgrid_tags = doc['searching_page_tags']['productgrid'][0]
+        productlist_tags = doc['searching_page_tags']['productlist'][0]
 
         toshow = []
-        for products in productlist:
-            title_tag = doc['searching_page_tags']['productname'][0]
-            image_tag = doc['searching_page_tags']['productimage'][0]
-            link_tag = doc['searching_page_tags']['producturl'][0]
-            price_tag = doc['searching_page_tags']['productprice']
+        try:
 
-            try:
-                eachproduct = {}
-                eachproduct['link'] = products.find_element(By.CLASS_NAME, link_tag['class']).get_attribute('href')
-                eachproduct['image'] = products.find_element(By.CLASS_NAME, image_tag['class']).get_attribute('src')
-                eachproduct['title'] = products.find_element(By.CLASS_NAME, title_tag['class']).text
-                eachproduct['price'] = products.find_element(By.CLASS_NAME, price_tag['class']).text
+            productgrid = WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.XPATH, productgrid_tags['xpath']))
+            )
+            productlist = productgrid.find_elements(By.CLASS_NAME, productlist_tags['class'])
 
-                toshow.append(eachproduct)
-            except Exception as e:
-                print(f"Error processing one product.")
-                # print(f"Error processing product: {e}")
-    except NoSuchElementException as noElementFound:
-        print("The Elements were not found")
+            for products in productlist:
+                title_tag = doc['searching_page_tags']['productname'][0]
+                image_tag = doc['searching_page_tags']['productimage'][0]
+                link_tag = doc['searching_page_tags']['producturl'][0]
+                price_tag = doc['searching_page_tags']['productprice']
+                ratings_tag = doc['searching_page_tags']['productratings']
+                no_of_ratings = doc['searching_page_tags']['product_no_of_ratings']
 
-    # Print product details
-    for product in toshow:
-        print(f"Link: {product['link']}")
-        print(f"Image: {product['image']}")
-        print(f"Title: {product['title']}")
-        print(f"Price: Rs.{product['price']}")
-        print("-" * 40)
-
-    print("Ending driver in 10 seconds")
-    sleep(10)
-
-    driver.quit()
+                try:
+                    eachproduct = {
+                        'title': products.find_element(By.CLASS_NAME, title_tag['class']).text,
+                        'platform': doc['name'],
+                        'link': products.find_element(By.CLASS_NAME, link_tag['class']).get_attribute('href'),
+                        'image': products.find_element(By.CLASS_NAME, image_tag['class']).get_attribute('src'),
+                        'price': prices_formatter(products.find_element(By.CLASS_NAME, price_tag['class']).text),
+                        'ratings': ratings(products.find_element(By.CLASS_NAME, ratings_tag['class']).text) if element_exists(products, By.CLASS_NAME, ratings_tag['class']) else 'NA',
+                        'no_of_ratings': products.find_element(By.CLASS_NAME,no_of_ratings['class']).text if element_exists(products,By.CLASS_NAME,no_of_ratings['class']) else 'NA'
+                    }
 
 
+                    toshow.append(eachproduct)
+                except Exception as e:
+                    print(f"Error processing one product.")
+                    # print(f"Error processing product: {e}")
+        except NoSuchElementException as noElementFound:
+            print("The Elements were not found")
+
+        # Print product details
+        for product in toshow:
+            print(f"Link: {product['link']}")
+            print(f"Image: {product['image']}")
+            print(f"Title: {product['title']}")
+            print(f"Price: Rs.{product['price']}")
+            print("-" * 40)
+            add_to_database(product)
+
+        print("Ending driver in 10 seconds")
+        sleep(10)
+
+        driver.quit()
 
 
+main_web_scrapper()
---
+++ WebScrapper/middlewares_fns.py
@@ -0,0 +1,5 @@
+def ratings(rating):
+    return rating[:3]
+
+def prices_formatter(price):
+    return price.replace(',', '')
